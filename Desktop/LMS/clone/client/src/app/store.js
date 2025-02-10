6311c7985b1dee11428144c6e745f8efb2732436
aneesrehman <115420485+aneesrehman89@users.noreply.github.com>
2025-02-10 11:12:44 +0500

    Delete Desktop directory

--- Desktop/LMS/clone/client/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./rootReducer";
-import { authApi } from "@/features/apis/authApi";
-
-
-export const appStore = configureStore({
-        reducer:rootReducer,
-        middleware:(defaultMiddleware) => defaultMiddleware().concat(authApi.middleware)
-})
--- Desktop/LMS/clone/server/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { loginn, registerr } from '../controllers/user.controller.js';
-
-const router = express.Router();
-
-router.route('/registerr').post(registerr)
-router.route('/loginn').post(loginn)
-
-export default router;
