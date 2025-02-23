def32e6161b863b2dbae85d597546b2dd35defbf
aneesrehman89 <miaan.anees@gmail.com>
2025-02-23 17:06:23 +0500

    setting home page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { TiThMenu } from "react-icons/ti";
+import { TiThMenu } from 'react-icons/ti';
 import Cookies from 'js-cookie';
-import { UserContext } from "../data/UserProvider";
+import { UserContext } from '../data/UserProvider';
 import logo from '../assets/homepg/headerLogo.png';
 import PP from '../assets/homepg/header-profile-pic.jpg';
 import '../css/Header.css';
@@ -14,50 +14,74 @@ const Header = () => {
   const handleLogout = () => {
     setUser(null);
     Cookies.remove('user');
-    navigate("/");
+    navigate('/');
   };
 
   const ViewProfile = () => {
-    navigate("/myProfile");
+    navigate('/myProfile');
   };
 
   return (
-    <header className="header-container">
-      <input type="checkbox" id="check" />
-      <label htmlFor="check" className="menu-icon">
+    <header className='header-container'>
+      <input type='checkbox' id='check' />
+      <label htmlFor='check' className='menu-icon'>
         <TiThMenu />
       </label>
 
-      <div className="header-logo">
-        <img src={logo} alt="Health Hub Logo" />
-        <h2>Health-Hub</h2>
+      <div className='header-logo'>
+        <Link to='/'>
+          <img src={logo} alt='Health Hub Logo' />
+          <h2>Health-Hub</h2>
+        </Link>
       </div>
 
-      <nav className="header-list">
+      <nav className='header-list'>
         <ul>
-          <li><Link to="/" className="link">Home</Link></li>
-          <li><Link to="/docAppointment" className="link">All Doctors</Link></li>
-          <li><Link to="/about" className="link">About</Link></li>
-          <li><Link to="/contact" className="link">Contact Us</Link></li>
+          <li>
+            <Link to='/' className='link'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/docAppointment' className='link'>
+              All Doctors
+            </Link>
+          </li>
+          <li>
+            <Link to='/about' className='link'>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to='/contact' className='link'>
+              Contact Us
+            </Link>
+          </li>
         </ul>
       </nav>
 
       {user ? (
-        <div className="dropdown">
-          <div className="profile">
-            <img src={PP} alt="User Profile" className="profile-pic" />
+        <div className='dropdown'>
+          <div className='profile'>
+            <img src={PP} alt='User Profile' className='profile-pic' />
             <h4>{user.name}</h4>
           </div>
-          <div className="dropdown-content">
+          <div className='dropdown-content'>
             <ul>
               <li onClick={ViewProfile}>Profile</li>
-              <li><Link to="/myappointment" className="link">My Appointment</Link></li>
+              <li>
+                <Link to='/myappointment' className='link'>
+                  My Appointment
+                </Link>
+              </li>
               <li onClick={handleLogout}>Logout</li>
             </ul>
           </div>
         </div>
       ) : (
-        <Link to="/login" className="login-btn">Login</Link>
+        <Link to='/login' className='login-btn'>
+          Login
+        </Link>
       )}
     </header>
   );
